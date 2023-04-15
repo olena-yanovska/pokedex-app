904d5ed2b938b6fc4a61c7dc2de7aed4f3537386
Olena Yanovska <alena.yn.adw@gmail.com>
2023-04-16 01:57:56 +0300

    loaded types of pokemons from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,36 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
 export const App: React.FC = () => {
   const [pokemons, setPokemons] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const API_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
-
-  function wait(delay: number) {
-    return new Promise(resolve => setTimeout(resolve, delay));
-  }
-  
-  function getPokemons() {
-    // keep this delay for testing purpose
-    return wait(500)
-      .then(() => fetch(API_URL))
-      .then(response => response.json());
-  }
+  const API_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
 
   const loadPokemons = async () => {
     try {
       setIsLoading(true);
 
-      const pokemons = await getPokemons();
-      setPokemons(pokemons.results);
-      console.log('pokemons', pokemons)
+      const result = await axios.get(API_URL);
+      loadPokemonData(result.data.results);
     } catch (error) {
       setIsError(true);
+      console.log('Error with loading')
     } finally {
       setIsLoading(false);
     }
   };
+
+  const loadPokemonData = async (result: any) => {
+    result.map(async (pokemon: any) => {
+      const result = await axios.get(pokemon.url);
+      
+      setPokemons(prev => [...prev, result.data])
+      console.log('pokemon data', result);
+    })
+  }
 
   useEffect(() => {
     loadPokemons();
@@ -49,7 +48,13 @@ export const App: React.FC = () => {
           <div className="pokemonsList">
             {pokemons && pokemons.map(pokemon => (
               <div className="pokemon">
+                <img
+                  src={pokemon.sprites.front_default}
+                  alt=""
+                />
                 <div>{pokemon.name}</div>
+                <div>{pokemon.types.map((pokemon: any) => pokemon.type.name)}
+                </div>
               </div>
             ))}
           </div>
