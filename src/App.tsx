fddf06d907a750ceee35f57f707c96dcb558713f
Olena Yanovska <alena.yn.adw@gmail.com>
2023-04-17 17:13:17 +0300

    added some styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const API_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=10';
+  const API_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=5';
 
   const loadPokemons = async () => {
     try {
@@ -24,12 +24,15 @@ export const App: React.FC = () => {
   };
 
   const loadPokemonData = async (result: any) => {
-    result.map(async (pokemon: any) => {
+    const promises = result.map(async (pokemon: any) => {
       const result = await axios.get(pokemon.url);
-      
-      setPokemons(prev => [...prev, result.data])
-      console.log('pokemon data', result);
-    })
+      console.log('result', result);
+      return result.data;
+    });
+
+    const pokemonData = await Promise.all(promises);
+    console.log('pokemonData', pokemonData);
+    setPokemons(pokemonData);
   }
 
   useEffect(() => {
@@ -38,25 +41,42 @@ export const App: React.FC = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Pokedex App</h1>
+      <header className="header">
+        <h1 className='header-text'>Pokedex</h1>
       </header>
       <main>
         {isLoading ? (
           <h1>Loading...</h1>
         ) : (
-          <div className="pokemonsList">
-            {pokemons && pokemons.map(pokemon => (
-              <div className="pokemon">
-                <img
-                  src={pokemon.sprites.front_default}
-                  alt=""
-                />
-                <div>{pokemon.name}</div>
-                <div>{pokemon.types.map((pokemon: any) => pokemon.type.name)}
+          <div className='pokemons-wrapper'>
+            <div className="pokemons-list-wrapper">
+            <div className="pokemons-list">
+              {pokemons && pokemons.map(pokemon => (
+                <div className="pokemon-card">
+                  <img
+                    className="pokemon-image"
+                    src={pokemon.sprites.front_default}
+                    alt=""
+                  />
+                  <div className='pokemon-name'>{pokemon.name}</div>
+                  <div className='pokemon-types'>
+                    {pokemon.types.map((pokemon: any) => {
+                      return (
+                        <button className="button is-success pokemon-type">{pokemon.type.name}</button>
+                      )
+                    })}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+
+            <button className='button is-link load-more-btn'>Load more</button>
+
+            </div>
+
+            <div className='pokemons-data'>
+              Pokemon Data
+            </div>
           </div>
         )}
       </main>
